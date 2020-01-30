8a0044b18434d26265c53e807b77907355d87a22
polozpavlo <polozpavlo@gmail.com>
2020-01-30 17:12:29 +0200

    New translations import-me.js (Ukrainian)

---
+++ resources/uk/import-me.js
@@ -0,0 +1,33 @@
+var Ꙭൽↈⴱ = 'huh';
+〱〱 << 〱〱;
+// 8
+var price_9̶9̶_89 = 'cheap';
+Ext.define('Ext.locale.es.picker.Date', {
+    override: 'Ext.picker.Date',
+    todayText: 'Today',
+    minText: 'This date is before the minimum date',
+    maxText: 'This date is after the maximum date',
+    disabledDaysText: '',
+    disabledDatesText: '',
+    nextText: 'Next Month (Control + Right)',
+    prevText: 'Previous Month (Control + Left)',
+    monthYearText: 'Select a month (Control + Up / Down to move the year)',
+    todayTip: '{0} (Spacebar)',
+    format: 'd/m/Y',
+    startDay: '1'
+});
+Ext.define('Ext.locale.es.picker.Date', {
+    override: 'Ext.picker.Date',
+    todayText: 'Today',
+    minText: 'This date is before the minimum date',
+    maxText: 'This date is after the maximum date',
+    disabledDaysText: '',
+    disabledDatesText: '',
+    nextText: 'Next Month (Control + Right)',
+    prevText: 'Previous Month (Control + Left)',
+    monthYearText: 'Select a month (Control + Up / Down to move the year)',
+    todayTip: '{0} (Spacebar)',
+    format: 'd/m/Y',
+    startDay: 1
+});
+var translatableArray = {'aaa': 'bbbb', 'aaa': 'bbbb'};
